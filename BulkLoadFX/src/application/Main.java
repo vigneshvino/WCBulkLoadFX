5f021284516acc74a78d23d831ce9a93e8a30c05
vvigneshwaran <vvigneshwaran@ITEM-S94761.emea.msad.sopra>
2020-11-13 14:31:32 +0530

    Commit on 13/11/2020 after converting this project into maven project

--- BulkLoadFX/src/application/Main.java
+++ BulkLoadFX/src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.fxml.FXMLLoader;
 
 
@@ -12,10 +13,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			HBox root = (HBox)FXMLLoader.load(getClass().getResource("Test.fxml"));
+			Scene scene = new Scene(root,645,550);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("CustomLoaderFX");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- BulkLoadFX/src/application/SampleController.java
+++ BulkLoadFX/src/application/SampleController.java
@@ -1,5 +1,34 @@
 package application;
 
-public class SampleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
+public class SampleController implements Initializable {
+	
+    @FXML
+    private Button dbTestConnection;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		dbTestConnection.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				System.out.println("Test Connection button is clicked..!!!");
+				Alert alert = new Alert(Alert.AlertType.INFORMATION);
+				alert.setTitle("Button Alert");
+				alert.show();
+			}
+		});
+	}
 	
 }
