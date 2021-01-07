7898f579bae5e27dfe140017cbf118a87de59c4a
Vignesh <vigneshvino@hotmail.com>
2021-01-07 23:28:15 +0530

    Update ExtractWTPart.java
    Added logic for writing WTPart and its subtypes to csv file

--- BulkLoadFX/src/com/soprasteria/extract/ExtractWTPart.java
+++ BulkLoadFX/src/com/soprasteria/extract/ExtractWTPart.java
@@ -1,14 +1,23 @@
 package com.soprasteria.extract;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.sql.Timestamp;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.TreeSet;
 
 import com.ptc.core.command.common.CommandException;
@@ -50,6 +59,7 @@ import wt.meta.LocalizedValues;
 import wt.method.RemoteAccess;
 import wt.method.RemoteMethodServer;
 import wt.part.WTPart;
+import wt.part.WTPartHelper;
 import wt.query.QuerySpec;
 import wt.query.SearchCondition;
 import wt.services.applicationcontext.implementation.DefaultServiceProvider;
@@ -59,6 +69,8 @@ import wt.type.TypedUtility;
 import wt.type.TypedUtilityServiceHelper;
 import wt.util.WTException;
 import wt.util.WTPropertyVetoException;
+import wt.vc.VersionControlException;
+import wt.vc.VersionControlHelper;
 
 public class ExtractWTPart implements RemoteAccess {
 
@@ -89,44 +101,170 @@ public class ExtractWTPart implements RemoteAccess {
 
 	}
 
-	public static void start(String type) throws WTException, RemoteException {
+	public static void start(String type) throws WTException, IOException {
 		// Export process starts from here
 		System.out.println("Object name from the application - " + type);
 		
 		// Get the soft types list for input type here
 		TypeIdentifier typeIdent = TypeHelper.getTypeIdentifier(type);
 		TypeIdentifier[] typeIdentArray = TypedUtilityServiceHelper.service.getSubtypes(typeIdent, true, true, null);
+		System.out.println("Total number of subtypes for the type - "+type+" is "+typeIdentArray.length);
+		
+		// Storing the part numbers of the sub types
+		Set<String> subTypesNumberSet = new HashSet<String>();
+		
+		// Querying the part subtypes and writing on  csv files
 		for(TypeIdentifier typeIdentifier:typeIdentArray) {
+			List<String> rows = new ArrayList<String>();
+			ArrayList<String> ibaList = new ArrayList<String>();
+			ArrayList<String> ibaNameList = new ArrayList<String>();
+			ArrayList<String> ibaValuesList = new ArrayList<String>();
+			
+			String typeName = null;
+			
+			HashMap<WTPart, ArrayList<String>> objectList = new HashMap<WTPart, ArrayList<String>>();
+			String typeDisplayName = TypedUtilityServiceHelper.service.getLocalizedTypeName(typeIdentifier, SessionHelper.manager.getLocale());
 			TypeDefinitionReference typeDefRef = TypedUtility.getTypeDefinitionReference(typeIdentifier.getTypeInternalName());
 			QuerySpec qspec = new QuerySpec(WTPart.class);
 			qspec.appendWhere(new SearchCondition(WTPart.class, "typeDefinitionReference.key.id", SearchCondition.EQUAL,typeDefRef.getKey().getId()),new int[] {0});
 			QueryResult qresult = PersistenceHelper.manager.find(qspec);
-			System.out.println("Number of parts in this type - "+typeIdentifier.getTypeInternalName()+" is "+qresult.size());
+			System.out.println("Number of parts in this type - "+typeDisplayName+" is "+qresult.size());
 			while (qresult.hasMoreElements()) {
 				WTPart part = (WTPart) qresult.nextElement();
-				System.out.println("Part Number - " + part.getNumber() + " Name - " + part.getName() + " Created On - "
-						+ part.getCreateTimestamp() +" Status - "+part.getCheckoutInfo()+" Modified By "+part.getModifierName()+ " Modified On - " + part.getModifyTimestamp() + " LC State - "
-						+ part.getLifeCycleState() + " Created By - " + part.getCreatorName());
+				typeName = TypedUtilityServiceHelper.service.getLocalizedTypeName(part, SessionHelper.manager.getLocale());
+				subTypesNumberSet.add(part.getNumber());
+				String entry = preparePartEntry(part);
+				System.out.println("Entry is "+entry);
 				IBAHolder ibaHolder = part;
-				getAttributeValueFromIBAHolder(ibaHolder,typeIdentifier.getTypeInternalName());
+				ibaList = getAttributeValueFromIBAHolder(ibaHolder);
+				// IBAList has the values of both IBAName and its value. Both are display identifiers.
+				// TODO check here for future change to add internal names instead of display names
+				for(String str:ibaList) {
+					String[] strArr = str.split(":");
+					String name = strArr[0];
+					// One time addition of iba names
+					if((ibaNameList.size() != ibaList.size()) ) {
+					ibaNameList.add(name);
+					}
+					String value = strArr[1];
+					if((ibaValuesList.size() != ibaList.size())) {
+					ibaValuesList.add(value);
+					}
+				}
+				
+			//	rows.add(ibaList);
+				System.out.println("ibaNameList is "+ibaNameList);
+				String ibaValues = String.join(";", ibaValuesList);
+				entry = entry + ibaValues;
+				rows.add(entry);
+				System.out.println("Entry after added iba values is "+entry);
+				System.out.println("Size of ibaList is "+ibaList.size()+" - "+ibaList);
+				ibaValuesList.clear();
 			}
+			// Pass the row list and iba list for csv writing
+			if(typeName != null) {
+			writeTocsvFile(rows,ibaNameList,typeName);
+			}
+			
 	//		TypeIdentifier typeIdentifier = TypeIdentifierUtility.getTypeIdentifier("wt.part.WTPart");
 			System.out.println("TypeIdentifier value - "+typeIdentifier+" getTypeName - "+typeIdentifier.getTypename());
-			getSoftAttributes(typeIdentifier, SessionHelper.manager.getLocale());
+	//		getSoftAttributes(typeIdentifier, SessionHelper.manager.getLocale());
 		}
 		
-/*		QuerySpec qspec = new QuerySpec(WTPart.class);
-
+		// Performing the query operation on normal wtparts and writing on csv file
+		
+		QuerySpec qspec = new QuerySpec(WTPart.class);
 		QueryResult qresult = PersistenceHelper.manager.find(qspec);
-		System.out.println("Query result size in wtpart class is " + qresult.size());*/
+		System.out.println("Query result size in wtpart class is " + qresult.size());
 		
+		List<String> rows_genericPart = new ArrayList<String>();
+		ArrayList<String> ibaList_genericPart = new ArrayList<String>();
+		ArrayList<String> ibaNameList_genericPart = new ArrayList<String>();
+		ArrayList<String> ibaValuesList_genericPart = new ArrayList<String>();
 		
-
+		while(qresult.hasMoreElements()) {
+			WTPart genericPart = (WTPart) qresult.nextElement();
+			if(!(subTypesNumberSet.contains(genericPart.getNumber()))) {
+				String entry = preparePartEntry(genericPart);
+				System.out.println("Entry for WTPart is "+entry);
+				IBAHolder ibaHolder = genericPart;
+				ibaList_genericPart = getAttributeValueFromIBAHolder(ibaHolder);
+				// IBAList has the values of both IBAName and its value. Both are display identifiers.
+				// TODO check here for future change to add internal names instead of display names
+				if(ibaList_genericPart.size() > 0) {
+					for(String str:ibaList_genericPart) {
+						String[] strArr = str.split(":");
+						String name = strArr[0];
+						// One time addition of iba names
+						if((ibaNameList_genericPart.size() != ibaList_genericPart.size()) ) {
+							ibaNameList_genericPart.add(name);
+						}
+						String value = strArr[1];
+						if((ibaValuesList_genericPart.size() != ibaList_genericPart.size())) {
+							ibaValuesList_genericPart.add(value);
+						}
+					}
+					
+					System.out.println("ibaNameList is "+ibaNameList_genericPart);
+					String ibaValues = String.join(";", ibaValuesList_genericPart);
+					entry = entry + ibaValues;
+					rows_genericPart.add(entry);
+					System.out.println("Entry after added iba values is "+entry);
+					System.out.println("Size of ibaList is "+ibaList_genericPart.size()+" - "+ibaList_genericPart);
+					ibaValuesList_genericPart.clear();
+					
+				} else {
+					rows_genericPart.add(entry);
+				}
+				
+				
+			//	rows.add(ibaList);
+				
+			}
+			
+		}
+		
+		String typeName = "WTPart";
+		writeTocsvFile(rows_genericPart,ibaNameList_genericPart,typeName);
 	}
 
-	private static void getAttributeValueFromIBAHolder(IBAHolder ibaHolder, String typeName) throws RemoteException, WTException {
+	private static String preparePartEntry(WTPart part) throws VersionControlException {
+		// TODO Auto-generated method stub
+		String entry = part.getNumber() + ";" + part.getName() + ";" + part.getType() + ";"
+				+ Boolean.toString(part.isEndItem()) + ";" + part.getDefaultTraceCode().toString() + ";"
+				+ part.getGenericType().toString() + ";" + part.getFolderPath() + ";"
+				+ part.getOrganizationName() + ";" + VersionControlHelper.getVersionIdentifier(part).getValue()
+				+ ";" + VersionControlHelper.getIterationDisplayIdentifier(part).toString() + ";"
+				+ part.getViewName() + ";" + part.getLifeCycleState().getDisplay() + ";" + part.getLifeCycleName() + ";" + part.getSource().getDisplay() + ";" + part.getDefaultUnit().getDisplay() + ";" + Boolean.toString(part.isCollapsible()) + ";" + part.getCreatorName() + ";" + part.getModifierName() + ";" + part.getCreateTimestamp().toGMTString() + ";" +
+				part.getModifyTimestamp().toGMTString() + ";";
+		return entry;
+	}
+
+	private static void writeTocsvFile(List<String> rows, ArrayList<String> ibaList, String typeName) throws IOException {
+		// TODO Auto-generated method stub
+		FileWriter csvWriter = new FileWriter(new File("C:\\dev\\ExtractedFiles\\"+typeName+".csv"));
+		csvWriter.append("NUMBER;NAME;OBJECTTYPE;ENDITEM;TRACECODE;GENERICTYPE;FOLDER_LOCATION;ORGANIZATION_ID;REVISION;ITERATION;VIEW;STATE;LIFECYCLE_TEMPLATE;SOURCE;DEFAULT_UNIT;COLLAPSIBLE;CREATED_BY;MODIFIED_BY;CREATED_DATE;MODIFIED_DATE");
+		csvWriter.append(";");
+		for (String ibaName:ibaList) {
+			csvWriter.append(ibaName);
+			csvWriter.append(";");
+		}
+		csvWriter.append("\n");
+		for(String entry:rows) {
+			csvWriter.append(entry);
+			csvWriter.append("\n");
+		}
+		
+		csvWriter.flush();
+		csvWriter.close();
+		
+		System.out.println("CSV File was generated successfully");
+	}
+
+	private static ArrayList<String> getAttributeValueFromIBAHolder(IBAHolder ibaHolder) throws RemoteException, WTException {
 		// TODO Auto-generated method stub
 		NumberFormat formatter = new DecimalFormat("#.########");
+		ArrayList<String> ibaValuesArrayList = new ArrayList<String>();
 		
 		ibaHolder = IBAValueHelper.service.refreshAttributeContainer(ibaHolder, null, SessionHelper.manager.getLocale(), null);
 		DefaultAttributeContainer attContainer = (DefaultAttributeContainer) ibaHolder.getAttributeContainer();
@@ -183,6 +321,13 @@ public class ExtractWTPart implements RemoteAccess {
 					if(attValue != null) {
 						AttributeTypeSummary ats = persAdapter.getAttributeDescriptor(internalName);
 						
+						// Getting the Display name of attribute's internal name
+			//			TypeIdentifier attTypeidentifier = TypedUtility.getTypeIdentifier(typeIdentifier);
+			//			TypeDefinitionReadView typeView = TypeDefinitionServiceHelper.service.getTypeDefView(typeIdentifier);
+						AttributeDefinitionReadView attView = TypeDefinitionServiceHelper.service.getAttributeDefView(ats.getAttributeTypeIdentifier());/*typeView.getAttributeByName(internalName);*/
+						String ibaDisplayNameValue = attView.getPropertyValueByName("displayName").getValueAsString();
+						System.out.println("IBADisplayName - "+ibaDisplayNameValue);
+						
 						/*AttributeDefinitionReadView adrv = TypeDefinitionServiceHelper.service.getAttributeDefView(ats.getAttributeTypeIdentifier());
 						System.out.println("adrv value is "+adrv);
 						if(adrv != null) {
@@ -200,22 +345,25 @@ public class ExtractWTPart implements RemoteAccess {
 						LWCEnumerationEntryValuesFactory eevf = new LWCEnumerationEntryValuesFactory();
 						LocalizedValues values = eevf.get(enumIdentifier, Locale.ENGLISH);
 						System.out.println("*** Display value is "+values.getDisplay());*/
+						String displayIBAValue = null;
 						if(legalValueSet instanceof EnumeratedSet) {
 							EnumeratedSet enumSet = (EnumeratedSet) legalValueSet.getIntersection(legalValueSet);
 							System.out.println("enumSet getElements value is "+enumSet.getElements());
 							if(enumSet != null) {
+								System.out.println("attValue here is "+attValue);
 								EnumerationEntryIdentifier identifier = enumSet.getElementByKey(attValue.toString());
 								System.out.println("identifier value is "+identifier);
 								if((identifier != null) && (identifier.getKey() != null)) {
 									String enumKey = (String) identifier.getKey();
 									if(attValue.equals(enumKey)) {
 										DefinitionDescriptor defValue = DESCRIPTOR_FACTORY.get(identifier, null, SessionHelper.manager.getLocale());
-										String displayIBAValue = defValue.getDisplay();
+										displayIBAValue = defValue.getDisplay();
 										System.out.println("*** display name of iba got through enum definition is "+displayIBAValue);
 									}
 								}
 							}
 						}
+						ibaValuesArrayList.add(ibaDisplayNameValue+":"+displayIBAValue);
 					}
 					// Getting iba display value using typename
 			/*		TypeIdentifier identifier = TypedUtility.getTypeIdentifier(typeName);
@@ -226,6 +374,7 @@ public class ExtractWTPart implements RemoteAccess {
 				}
 			}
 		}
+		return ibaValuesArrayList;
 	}
 
 	private static void getSoftAttributes(TypeIdentifier typeIdentifier, Locale locale) throws WTException {
